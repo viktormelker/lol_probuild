ff5fc53efa709efbec9828df1d03c46e550e4abd
Viktor Melker <viktormelker@gmail.com>
2023-06-02 04:59:04 +0200

    Update root layout

--- probuild_ex/assets/js/app.js
+++ probuild_ex/assets/js/app.js
@@ -42,3 +42,17 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
+// tailwind ui mobile nav
+const $toggleMenu = document.getElementById('toggle-menu')
+const $burger = document.getElementById('burger')
+const $xMark = document.getElementById('x-mark')
+const $mobileMenu = document.getElementById('mobile-menu')
+
+$toggleMenu.addEventListener('click', (event) => {
+  event.preventDefault()
+  ;['hidden', 'block'].forEach((className) => {
+    $burger.classList.toggle(className)
+    $xMark.classList.toggle(className)
+  })
+  $mobileMenu.classList.toggle('hidden')
+})
