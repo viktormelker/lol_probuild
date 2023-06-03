edf3d6cc2f5bab772614b6e92eba50b60eba3ecd
Viktor Melker <viktormelker@gmail.com>
2023-06-03 09:57:16 +0200

    Add base live view page

--- probuild_ex/lib/probuild_ex/app.ex
+++ probuild_ex/lib/probuild_ex/app.ex
@@ -10,18 +10,19 @@ defmodule ProbuildEx.App do
   alias ProbuildEx.Games.Participant
 
   def list_pro_participant_summoner(_opts) do
-    from participant in Participant,
-      left_join: game in assoc(participant, :game)
-      left_join: summoner in assoc(participant, :summoner),
-      left_join: opponent_participant in assoc(participant, :opponent_participant),
-      inner_join: pro in assoc(summoner, :pro),
-      preload: [
-        game: game,
-        opponent_participant: opponent_participant,
-        summoner: {summoner, pro: pro},
-      ],
-      order_by: [desc: game.creation],
-      limit: 20
+    query =
+      from participant in Participant,
+        left_join: game in assoc(participant, :game),
+        left_join: summoner in assoc(participant, :summoner),
+        left_join: opponent_participant in assoc(participant, :opponent_participant),
+        inner_join: pro in assoc(summoner, :pro),
+        preload: [
+          game: game,
+          opponent_participant: opponent_participant,
+          summoner: {summoner, pro: pro}
+        ],
+        order_by: [desc: game.creation],
+        limit: 20
 
     Repo.all(query)
   end
