5410182a473bcdc892aeb8c7631b64c442330206
Viktor Melker <viktormelker@gmail.com>
2023-07-16 18:27:44 +0200

    Fix test failures?

--- probuild_ex/mix.exs
+++ probuild_ex/mix.exs
@@ -35,7 +35,7 @@ defmodule ProbuildEx.MixProject do
     [
       {:phoenix, "~> 1.6.15"},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
+      {:ecto_sql, "== 3.8.3"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
